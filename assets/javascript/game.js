3829f63d912c75466b2d7e2eb4c81e5caf7ca373
Alan Kemsley <alanjkemsley@gmail.com>
2017-07-19 15:59:17 -0400

    Fix responsive design.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,9 +44,6 @@ $(document).ready(function(){
     }
   ];
 
-  //GAME INSTRUCTIONS
-  alert("INSTRUCTIONS: ");
-
   //PLAYER AND OPPONENT CHARACTER SELECTION PROCESS
   $(".rebelsList").on({
     "click": function(){
@@ -64,7 +61,6 @@ $(document).ready(function(){
           $(this).children("h6").text("");
           $("#vs").css("color", "#EAE5D4");
           $("#restart").css("color", "#EAE5D4").css("border-color", "#EAE5D4");
-          console.log("Player: " + player);
 
           //If the player character has already been selected:
         } else {
@@ -76,7 +72,6 @@ $(document).ready(function(){
             $(this).children().css("border-color", "#555");
             $(this).children("h6").text("");
             $("#attack").css("color", "#A18064").css("border-color", "#A18064");
-            console.log("Opponent: " + opponent);
           }
 
       //ASSIGN SELECTED PLAYER CHARACTERS TO CORRESPONDING OBJECTS
@@ -101,9 +96,6 @@ $(document).ready(function(){
           break;
       }
 
-      //FOR TESTING
-      console.log("Player stats: " + player.health + " health, " + player.power + " attack power.");
-
       //ASSIGN SELECTED OPPONENT CHARACTERS TO CORRESPONDING OBJECTS
       switch (opponent) {
         case "JYN":
@@ -125,9 +117,6 @@ $(document).ready(function(){
           opponent = rebels[5];
           break;
       }
-
-      //FOR TESTING
-      console.log("Opponent stats: " + opponent.health + " health, " + opponent.counter + " counter-attack power.");
     }
   });
 
@@ -136,7 +125,6 @@ $(document).ready(function(){
     "click": function (){
       //IF PLAYER LOSES
       if (player.health <= 0) {
-        console.log("You have been defeated!");
         $("#alert").text("GAME OVER");
         $("#row2").css("opacity", "0.2");
         $("#row3").css("opacity", "0.2");
@@ -144,7 +132,6 @@ $(document).ready(function(){
         $(this).css("opacity", "0.2");
           //IF PLAYER WINS
       }   else if (opponent.health <= 0){
-            console.log("You have defeated your opponent!");
             $("#alert").text("You have Defeated Your Opponent.");
             $(this).on({"click": function(){$("#alert").text("Select Your Next Opponent.")}});
             $(this).css("opacity", "0.2");
@@ -157,8 +144,6 @@ $(document).ready(function(){
                 player.health = player.health - opponent.counter;
                 player.power = player.power * 2;
                 opponent.health = opponent.health - player.power;
-                console.log("Player stats: " + player.health + " health, " + player.power + " attack power.");
-                console.log("Opponent stats: " + opponent.health + " health, " + opponent.counter + " counter-attack power.");
                 $("#alert").text("Your Health: " + player.health + " | Opponent Health: " + opponent.health);
 
                 //Replace player and opponent health point text in HTML with new health points. I can't do this because I had previously cloned the selected characters into the new DIV, rather than moving them over. This also cloned the IDs so now I can't call the IDs of the new DIV. This is what I get for going the extra mile and trying to do more than what is expected of me.
@@ -173,7 +158,7 @@ $(document).ready(function(){
     "click": function (){
       if (player !== null) {
         location.reload();
-      } else {}
+      }
     }
   });
 
