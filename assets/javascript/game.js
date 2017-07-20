73ac8a926e74ad2a72359be91e59f8504053b1dc
Alan Kemsley <alanjkemsley@gmail.com>
2017-07-20 11:30:25 -0400

    Complete game. May need more gameplay balance.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,44 +3,45 @@ $(document).ready(function(){
   //CURRENT PLAYER AND OPPONENT CHARACTERS
   var player = null;
   var opponent = null;
+  var opponentsFought = [];
 
   //ARRAY OF CHARACTERS AS OBJECTS
   var rebels = [
     {
       name: "JYN",
       health: 100,
-      power: 5,
-      counter: 20
+      power: 4,
+      counter: 5
     },
     {
       name: "CASSIAN",
       health: 110,
-      power: 4,
-      counter: 15
+      power: 3,
+      counter: 4
     },
     {
       name: "K2",
       health: 130,
-      power: 3,
-      counter: 25
+      power: 2,
+      counter: 7
     },
     {
       name: "CHIRRUT",
       health: 90,
-      power: 7,
-      counter: 35
+      power: 6,
+      counter: 3
     },
     {
       name: "BAZE",
       health: 120,
-      power: 6,
-      counter: 30
+      power: 5,
+      counter: 6
     },
     {
       name: "BODHI",
       health: 80,
-      power: 2,
-      counter: 5
+      power: 1,
+      counter: 2
     }
   ];
 
@@ -54,8 +55,7 @@ $(document).ready(function(){
           player = $(this).children("h4").text();
           console.log("Player: " + player);
           $("#alert").text("Select Your Opponent");
-          //For better visual aesthetics, selected characters get grayed out instead of disappearing from the grid.
-          $(this).clone().replaceAll("#player-div").removeClass("col-md-1 rebelsList cards").addClass("player-div");
+          $(this).clone().replaceAll(".player-div").removeClass("col-md-1 rebelsList cards").addClass("player-div");
           $(this).css("opacity", "0.5");
           $(this).children().css("color", "#555");
           $(this).children().css("border-color", "#555");
@@ -65,14 +65,16 @@ $(document).ready(function(){
           //If the player character has already been selected:
         } else {
             opponent = $(this).children("h4").text();
+            opponentsFought.push(opponent);
             console.log("Opponent: " + opponent);
             $("#alert").text("Click ATTACK to Duel");
-            $(this).clone().replaceAll("#computer-div").removeClass("col-md-1 rebelsList cards").addClass("computer-div");
+            $(this).clone().replaceAll(".computer-div").removeClass("col-md-1 rebelsList cards").addClass("computer-div");
             $(this).css("opacity", "0.5");
             $(this).children().css("color", "#555");
             $(this).children().css("border-color", "#555");
             $(this).children("h6").css("color", "black");
-            $("#attack").css("color", "#EAE5D4").css("border-color", "#EAE5D4");
+            $(".button").css("color", "#EAE5D4").css("border", "3px solid #EAE5D4").css("opacity", "1");
+            console.log(opponentsFought);
           }
 
       //ASSIGN SELECTED PLAYER CHARACTERS TO CORRESPONDING OBJECTS
@@ -122,41 +124,43 @@ $(document).ready(function(){
   });
 
   //ATTACK PROCESS
-  $("#attack").on({
+  $(".button").on({
     "click": function (){
-      //IF PLAYER LOSES
-      if (player.health <= 0) {
-        $("#alert").text("GAME OVER");
-        $("#row2").css("opacity", "0.2");
-        $("#row3").css("opacity", "0.2");
-        $(this).on({"click": function(){$("#alert").text("Click RESTART to restart the game.")}});
-        $(this).css("opacity", "0.2");
-          //IF PLAYER WINS
-      }   else if (opponent.health <= 0){
-            $("#alert").text("You have Defeated Your Opponent.");
-            $(this).on({"click": function(){$("#alert").text("Select Your Next Opponent.")}});
-            $(this).css("opacity", "0.2");
+      if (opponent === null){
+      } else if (player.health <= 0 && opponent.health > 0){
+         $("#alert").text("GAME OVER");
+         $("#row2").css("opacity", "0.2");
+         $("#row3").css("opacity", "0.2");
+         $(".button").css("opacity", "0.2");
+       } else if (opponentsFought.length < 5 && opponent.health <= 0){
+            $("#alert").text("Select Your Next Opponent");
+            $(".button").css("opacity", "0.2");
+            $(".computer-div").css("opacity", "0.5");
+            $(".computer-div").children().css("color", "#555");
+            $(".computer-div").children().css("border-color", "#555");
+            $(".computer-div").children("h6").html("DEFEATED").css("color", "#AE3232");
             opponent = null;
-            console.log ("Player: " + player);
-            console.log("Opponent: " + opponent)
-              //IF NOBODY HAS WON YET
-          }   else {
-                player.health = player.health - opponent.counter;
-                opponent.health = opponent.health - player.power;
-                player.power = player.power + player.power;
-                $(".player-div").children("h6").html("<i class = 'fa fa-heartbeat hp-icon' aria-hidden='true'></i>" + " " + player.health)
-                $(".computer-div").children("h6").html("<i class = 'fa fa-heartbeat hp-icon' aria-hidden='true'></i>" + " " + opponent.health)
+          } else if (opponentsFought.length === 5 && opponent.health <= 0){
+              $("#alert").text("YOU WIN");
+              $("#row2").css("opacity", "0.2");
+              $("#row3").css("opacity", "0.2");
+              $(".button").css("opacity", "0.2");
+            } else {
+                player.health = Math.round(player.health - opponent.counter);
+                opponent.health = Math.round(opponent.health - player.power);
+                player.power = player.power * 1.1;
+                if (player.health > 0) {
+                  $(".player-div").children("h6").html("<i class = 'fa fa-heartbeat hp-icon' aria-hidden='true'></i>" + " " + player.health);
+                } else {
+                    $(".player-div").children("h6").html("<i class = 'fa fa-heartbeat hp-icon' aria-hidden='true'></i>" +  " DANGER ZONE").css("color", "#AE3232");
+                  }
+                if (opponent.health > 0) {
+                  $(".computer-div").children("h6").html("<i class = 'fa fa-heartbeat hp-icon' aria-hidden='true'></i>" + " " + opponent.health);
+                } else {
+                    $(".computer-div").children("h6").html("<i class = 'fa fa-heartbeat hp-icon' aria-hidden='true'></i>" + " DANGER ZONE").css("color", "#AE3232");
+                  }
               }
     }
   });
-
-
-
-
-
-
-
-
-
 
 });
